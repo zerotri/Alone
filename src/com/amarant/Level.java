14e4827d78ca92198228fe49828449e959bd0a6d
Shelby Munsch <smunsch@gmail.com>
2011-12-17 13:43:20 -0800

    quick exception handler

--- src/com/amarant/Level.java
+++ src/com/amarant/Level.java
@@ -6,9 +6,17 @@ public class Level {
 	private Block[] grid;
 	private int lwidth;
 	private int lheight;
+	private int gridwidth;
+	private int gridheight;
 	
 	public Level(String ref) {
 		// Load level from file ref
+		try {
+			
+		}
+		catch (Exception e) {
+			
+		}
 	}
 	
 	
